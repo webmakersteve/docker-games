3aefdc6fd40e19f0bb6c157d7859bf141c1fd572
Stephen Parente <webmakersteve@gmail.com>
2016-12-02 18:18:10 -0800

    Initial commit

---
+++ ark/run_ark.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+./ShooterGame/Binaries/Linux/ShooterGameServer TheIsland?listen?SessionName=$SERVER_NAME?ServerPassword=$SERVER_PASSWORD?ServerAdminPassword=$SERVER_ADMIN_PASSWORD -server -log
