3e6816b34c6f50d48f34010251ddddf10b5b88ee
Stephen Parente <webmakersteve@gmail.com>
2016-12-02 22:58:58 -0800

    Added terraria

---
+++ terraria/run_terraria.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# We have the base config.
+cp serverconfig.txt.base serverconfig.txt
+
+# Replace parameters with env vars
+sed -i "s|{WORLD_NAME}|$WORLD_NAME|g" serverconfig.txt
+sed -i "s|{DIFFICULTY}|$DIFFICULTY|g" serverconfig.txt
+sed -i "s|{PASSWORD}|$PASSWORD|g" serverconfig.txt
+
+# Run it with that config
+mono ./Linux/TerrariaServer.exe -config ./serverconfig.txt -world /home/steam/terraria/Worlds/world1.wld
